4f5cd14f9a9c9ed5edac1e9d05a8cdbd54f07a9b
Raffi Abberbock <rabberbock@gmail.com>
2023-11-27 15:00:52 -0500

    Test Bulk Insert

---
+++ Migrations/20231127195040_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EFGetStarted.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    Metadata = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    BlogId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    PostId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comment_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_PostId",
+                table: "Comment",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddDbContext<BloggingContext>();
+var services = serviceCollection.BuildServiceProvider();
+
+var blogs = new List<Blog>()
+{
+    new Blog()
+    {
+        Url = "https://www.example.com/1",
+        Posts = new List<Post>()
+        {
+            new Post()
+            {
+                Title = "Test",
+                Content = "Sample Content",
+                Blog = new Blog()
+                {
+                    Url = "https://www.example.com/2"
+                },
+                Comments = new List<Comment>()
+                {
+                    new Comment()
+                    {
+                        CommentId = 1000,
+                        Text = "Example"
+                    },
+                    new Comment()
+                    {
+                        CommentId = 1001,
+                        Text = "Example 2"
+                    }
+                }
+            }
+        }
+    },
+    new Blog()
+    {
+        Url = "https://www.example.com",
+        Posts = new List<Post>
+        {
+            new Post()
+            {
+                Title = "Test",
+                Content = "Sample Content",
+                Blog = new Blog()
+                {
+                    Url = "https://www.example.com/2"
+                }
+            }
+        }
+    }
+};
+
+try
+{
+    var context = services.GetRequiredService<BloggingContext>();
+    await context.BulkInsertAsync(blogs, opt => opt.IncludeGraph = true);
+    
+    // Saving regularly via EF works
+    // context.AddRange(blogs);
+    // await context.SaveChangesAsync();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=test_db;Username=postgres;Password=password");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Blog>()
+            .OwnsOne(b => b.Metadata, m =>
+            {
+                m.ToJson();
+            });
+    }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; set; }
+
+    public BlogMetadata Metadata { get; set; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+    public List<Comment> Comments { get; set; }
+}
+
+public class Comment
+{
+    public int CommentId { get; set; }
+    public string Text { get; set; }
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+}
+
+
+public class BlogMetadata
+{
+    public bool IsPublic { get; set; }
+}
